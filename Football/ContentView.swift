4c50cc5a37d7843b0cd992aed7eb704a3fc79455
jasonr1902 <jason.royer1902@gmail.com>
2025-01-25 18:37:46 -0600

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Jason Royer on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("What is Football to You?")
+                .font(.largeTitle)
+                .fontWeight(.thin)
+                .foregroundStyle(.green)
+            
+            HStack {
+                Image(systemName: "figure.american.football")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.blue)
+                Image(systemName: "figure.australian.football")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .foregroundStyle(.indigo)
+                Image(systemName: "figure.soccer")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.purple)
+                
+            }
+        }
+        .padding()
+        //.frame()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jason Royer on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
